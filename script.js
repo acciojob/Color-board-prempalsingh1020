12f5ad6a26f00187afdf710240ef3e5fdf01da0f
prempalsingh1020 <prempalsingh030499@gmail.com>
2026-02-13 15:45:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
 //your JS code here. If required.
+
+
+let mainDIV = document.querySelector(".container");
+
+//  create 800 boxes;
+
+let s= 800;  // no of the boxes
+
+for(let i=1;i<=s;i++){
+
+	let square = document.querySelector("div");
+	square.classList.add("square");
+
+	square.addeventListener("")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
