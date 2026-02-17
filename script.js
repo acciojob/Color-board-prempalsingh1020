c361d9bdfccb3371acd896b0e849fd9aa5110d9f
prempalsingh1020 <prempalsingh030499@gmail.com>
2026-02-17 14:22:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,28 +12,35 @@ for(let i=1;i<=s;i++){
 	let square = document.querySelector("div");
 	square.classList.add("square");
 
-	square.addeventListener("")
+	square.addEventListener("mouseeneter",()=>{
 
+		let color = changeTOcolor()
+		square.style.backgroundColor=color;
+		
+	});
 
+	square.addEventListener("mouseleave",()=>{
+		
+		setTimeout(()={
 
+			square.style.backgroundColor=black;
+			
+		},1000);
+	})
 
-
-
-
-
-
-
-
-
-
-
-
-
+		mainDIV.appendChild(square);
 
 	
 }
 
-
+function changeTOcolor() {
+	let c = "0123456789ABCDFE";
+	let s = "#";
+	for(let i=0;i<6;i++){
+		s += c[Math.floor(Math.random()*16)];
+	}
+	return s;
+}
 
 
 
